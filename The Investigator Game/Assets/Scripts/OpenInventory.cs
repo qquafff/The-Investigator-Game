b02c3376e4260850b58fa817b4bc5ac1be1aa56f
Dani <dani.niskanen@student.careeria.fi>
2021-03-24 12:09:01 +0200

    Inventory script made

---
+++ The Investigator Game/Assets/Scripts/OpenInventory.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class OpenInventory : MonoBehaviour
+{
+    public static bool InventoryOpen = false;
+
+    public GameObject InventoryUI;
+    public cameraController camCtrl;
+    public characterController charCtrl;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (InventoryOpen)
+            {
+                Close();
+            } else
+            {
+                Open();
+            }
+     
+        }
+    }
+    public void Close()
+    {
+        InventoryUI.SetActive(false);
+        //Time.timeScale = 1f;
+        InventoryOpen = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        camCtrl.sensitivity = 5f;
+        charCtrl.speed = 10f;
+    }
+
+    void Open()
+    {
+        InventoryUI.SetActive(true);
+        //Time.timeScale = 0f;
+        InventoryOpen = true;
+        Cursor.lockState = CursorLockMode.None;
+        camCtrl.sensitivity = 0f;
+        charCtrl.speed = 0f;
+    }
+}
